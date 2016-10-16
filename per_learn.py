29f5db5168073e4719be47b76ed1aa31fe12992d
vaishnavijanardhan <vjanardh@usc.edu>
2016-10-16 14:19:25 -0700

    code working, but refactor reqd

--- per_learn.py
+++ per_learn.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import json
+from random import shuffle
+from collections import Counter
 
 def learnModel(diction,data):
     for each_file in data:
@@ -13,6 +15,71 @@ def learnModel(diction,data):
                 else:
                     diction[each_token] = 1
             filest.close()
+
+def readWordFeatures(labelFilePathTuple):
+    wordFeatures = {}
+    f = labelFilePathTuple[1]
+    try:
+        filestream = open(f,"r",encoding="latin1")
+        content = filestream.read()
+        tokens = content.split()
+        wordFeatures = dict(Counter(tokens))
+    except:
+        print("Could not process file {0}".format(f))
+    finally:
+        filestream.close()
+    return wordFeatures
+
+def readAllWordFeatures(HamData, SpamData ):
+    """
+    read word frequency of all document
+    """
+    labelFilePathTuples = HamData + SpamData
+    fileFeatureDict = {}
+
+    for t in labelFilePathTuples:
+        wordFeatures = readWordFeatures(t)
+        fileFeatureDict[t[1]] = wordFeatures
+    return fileFeatureDict
+
+def trainPerceptron(labelFilePathTuples,fileFeatureDict,maxIteration=20):
+    """
+    For Ham, set Label = -1, for Spam, set Label = +1
+
+    return (weights,b)
+    """
+    weights = {}
+    b = 0
+    for i in range(0,maxIteration):
+        #randomize file index
+        shuffle(labelFilePathTuples)
+
+        for t in labelFilePathTuples:
+            #f - filepath
+            f = t[1]
+
+            if t[0] == "ham":
+                trueLabel = -1
+            else:
+                trueLabel = 1
+
+            wordCounts = fileFeatureDict[f]
+
+            alpha = 0
+            for word, wordCount in wordCounts.items():
+                if(word not in weights):
+                    weights[word] = 0
+                wordWeight = weights[word]
+
+                alpha += wordWeight*wordCount
+            #alpha is a prediction result
+            alpha += b
+            if(trueLabel * alpha <= 0):
+                for word in wordCounts.keys():
+                    weights[word] = weights[word] + trueLabel*wordCounts[word]
+                b = b + trueLabel
+
+    return (weights,b)
 
 #list of all files
 HamData = []
@@ -32,3 +99,16 @@ for drctry, drctry_name, file_name in os.walk(sys.argv[1]):
 
 hamfiles_total=len(HamData)
 spamfiles_total=len(SpamData)
+
+HamData = [("ham",filepath) for filepath in HamData]
+SpamData = [("spam",filepath) for filepath in SpamData]
+
+fileFeatureDict = readAllWordFeatures(HamData, SpamData)
+
+labelFilePathTuples = HamData + SpamData
+parameters = trainPerceptron(labelFilePathTuples,fileFeatureDict)
+
+with open('per_model.txt','w') as writefile:
+    json.dump(parameters,writefile)
+
+print("Done")
