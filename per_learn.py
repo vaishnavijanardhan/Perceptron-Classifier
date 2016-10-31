b56cb9989f90911dea40b87569727eb226e52357
vaishnavijanardhan <vjanardh@usc.edu>
2016-10-31 13:25:07 -0700

    Complete

--- per_learn.py
+++ per_learn.py
@@ -4,55 +4,55 @@ import random
 import json
 from collections import Counter
 
-def readAllWordFeatures(SpamData, HamData):
+def func_read_features(SpamData, HamData):
 
-    labelFilePathTuples = SpamData + HamData
-    fileFeatureDict = {}
-    wordFeatures = {}
+    total_training_files = SpamData + HamData
+    dict_features = {}
+    word_features = {}
 
-    for t in labelFilePathTuples:
-        f = t[1]
+    for each_file in total_training_files:
+        f = each_file[1]
         filestream = open(f,"r",encoding="latin1")
-        content = filestream.read()
-        tokens = content.split()
-        wordFeatures = dict(Counter(tokens))
+        file_content = filestream.read()
+        tokens = file_content.split()
+        word_features = dict(Counter(tokens))
         filestream.close()
-        fileFeatureDict[t[1]] = wordFeatures
-    return fileFeatureDict
+        dict_features[each_file[1]] = word_features
+    return dict_features
 
-def trainPerceptron(HamData,SpamData,fileFeatureDict):
+def func_perceptron(HamData,SpamData,dict_features):
 
-    labelFilePathTuples = HamData + SpamData
-    random.shuffle(labelFilePathTuples)
+    total_training_files = HamData + SpamData
+    random.shuffle(total_training_files)
     weights = {}
     for i in range(0,20):
-        for t in labelFilePathTuples:
-            if t[0] == "spam":
-                trueLabel = 1
+        for each_file in total_training_files:
+            if each_file[0] == "spam":
+                file_label = 1
             else:
-                trueLabel = -1
+                file_label = -1
 
-            wordCounts = fileFeatureDict[t[1]]
+            number_of_words = dict_features[each_file[1]]
 
-            alpha = 0
-            for word, wordCount in wordCounts.items():
+            result = 0
+            for word, word_count in number_of_words.items():
                 if(word not in weights):
                     weights[word] = 0
                 else:
-                    wordWeight = weights[word]
-                    temp_var = wordCount*wordWeight
-                    alpha =  alpha + temp_var
+                    w_word = weights[word]
+                    temp_var = word_count*w_word
+                    result =  result + temp_var
 
 
-            b = 0
-            alpha = alpha + b
+            temp_factor = 0
+            result = result + temp_factor
 
-            if((trueLabel * alpha > 0) != 1):
-                for word in wordCounts.keys():
-                    weights[word] = trueLabel*wordCounts[word] + weights[word]
-                b = trueLabel + b
+            if((file_label * result > 0) != 1):
+                for word in number_of_words.keys():
+                    weights[word] = file_label*number_of_words[word] + weights[word]
+                temp_factor = file_label + temp_factor
 
-    return (weights,b)
+    return (weights,temp_factor)
 
 
 def getHam():
@@ -61,7 +61,7 @@ def getHam():
 def getSpam():
     return [("spam",filepath) for filepath in SpamData]
 
-""" *********** START HERE *************** """
+""" ***** START HERE ***** """
 HamData = []
 SpamData = []
 
@@ -77,7 +77,7 @@ for drctry, drctry_name, file_name in os.walk(sys.argv[1]):
             else:
                  SpamData.append(os.path.join(drctry,every_file))
 
-""" # Training the learner with only 10% of the data
+""" Training the learner with only 10% of the data
 total_files = len(HamData) + len(SpamData)
 total_train_files = int(round(0.1*total_files*0.5))
 HamData = random.sample(HamData,total_train_files)
@@ -86,11 +86,8 @@ SpamData = random.sample(SpamData,total_train_files) """
 HamData = getHam()
 SpamData = getSpam()
 
-print(len(HamData))
-print(len(SpamData))
+dict_features = func_read_features(HamData, SpamData)
+model_file = func_perceptron(HamData, SpamData, dict_features)
 
-fileFeatureDict = readAllWordFeatures(HamData, SpamData)
-parameters_model = trainPerceptron(HamData, SpamData, fileFeatureDict)
-
-with open('per_model.txt','w') as writefile:
-     json.dump(parameters_model,writefile)
+with open('per_model.txt','w') as output_file:
+     json.dump(model_file,output_file)
