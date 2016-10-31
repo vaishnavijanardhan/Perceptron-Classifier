09eafb2a31bf592f8ef953eee758fa2522bf2886
vaishnavijanardhan <vjanardh@usc.edu>
2016-10-31 13:25:19 -0700

    Complete

--- per_classify.py
+++ per_classify.py
@@ -3,62 +3,62 @@ import os
 import sys
 from collections import Counter
 
-def readAllWordFeatures(filelpaths):
-    fileFeatureDict = {}
-    wordFeatures = {}
-    for f in filelpaths:
+def func_read_features(total_files):
+    dict_features = {}
+    word_features = {}
+    for f in total_files:
         filestream = open(f,"r",encoding="latin1")
-        content = filestream.read()
-        tokens = content.split()
-        wordFeatures = dict(Counter(tokens))
-        fileFeatureDict[f] = wordFeatures
-    return fileFeatureDict
+        file_content = filestream.read()
+        tokens = file_content.split()
+        word_features = dict(Counter(tokens))
+        dict_features[f] = word_features
+    return dict_features
 
-def predict(filepath,fileFeatureDict,alpha):
-    wordCounts = fileFeatureDict[filepath]
-    for word, wordCount in wordCounts.items():
+def func_classify(total_f,dict_features,result):
+    number_of_words = dict_features[total_f]
+    for word, word_count in number_of_words.items():
         if(word in weights):
-            wordWeight = weights[word]
+            w_word = weights[word]
         else:
-             wordWeight = 0
+             w_word = 0
 
-        temp_var = wordWeight*wordCount
-        alpha = temp_var + alpha
-    prediction = alpha + b
+        temp_var = w_word*word_count
+        result = temp_var + result
+    prediction = result + temp_fac
 
     return "spam" if prediction > 0 else "ham"
 
-""" Start here **************** """
+""" ***** Start here ***** """
 
-writeFilePath = sys.argv[2]
-writeContent = ""
+write_file = sys.argv[2]
+output_content = ""
 
 with open('per_model.txt') as parameterfile:
     parameters = json.load(parameterfile)
 
-global weights, b
+global weights, temp_fac
 weights = parameters[0]
-b = parameters[1]
+temp_fac = parameters[1]
 
-#files to be predicted
+
 all_files = []
 for drctry, drctry_name, file_name in os.walk(sys.argv[1]):
     for every_file in file_name:
         if ".txt" in every_file:
             all_files.append(os.path.join(drctry,every_file))
 
-fileFeatureDict = readAllWordFeatures(all_files)
+dict_features = func_read_features(all_files)
 
-alpha = 0
-predictedLabels = []
+result = 0
+do_classify = []
 for f in all_files:
-    predictedLabel = predict(f,fileFeatureDict,alpha)
-    predictedLabels = predictedLabels + [predictedLabel]
+    belongs_to = func_classify(f,dict_features,result)
+    do_classify = do_classify + [belongs_to]
 
-outputfile = open(writeFilePath,"w", encoding="latin1")
+out_file = open(write_file,"w", encoding="latin1")
 
-for result in zip(all_files,predictedLabels):
-    writeContent = "{0} {1}".format(result[1],result[0]) + "\n" + writeContent
+for result in zip(all_files,do_classify):
+    output_content = "{0} {1}".format(result[1],result[0]) + "\n" + output_content
 
-outputfile.write(writeContent)
-outputfile.close()
+out_file.write(output_content)
+out_file.close()
